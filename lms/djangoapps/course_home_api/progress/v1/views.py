84a725c26952b50bbf12c3aefa1eba5f9b265499
Daphne Li-Chen <dli-chen@edx.org>
2020-06-25 15:02:39 -0400

    AA-202: updated file imports

--- lms/djangoapps/course_home_api/progress/v1/views.py
+++ lms/djangoapps/course_home_api/progress/v1/views.py
@@ -2,7 +2,6 @@
 Dates Tab Views
 """
 
-from rest_framework import status
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -11,12 +10,7 @@ from edx_django_utils import monitoring as monitoring_utils
 from opaque_keys.edx.keys import CourseKey
 
 from lms.djangoapps.course_home_api.progress.v1.serializers import ProgressTabSerializer
-from lms.djangoapps.courseware.context_processor import user_timezone_locale_prefs
-from lms.djangoapps.courseware.courses import get_course_date_blocks, get_course_with_access
-from lms.djangoapps.courseware.date_summary import TodaysDate, verified_upgrade_deadline_link
-from lms.djangoapps.course_home_api.dates.v1.serializers import DatesTabSerializer
-from lms.djangoapps.course_home_api.toggles import course_home_mfe_dates_tab_is_active
-from openedx.features.course_experience.utils import dates_banner_should_display
+from lms.djangoapps.courseware.courses import get_course_with_access
 from openedx.features.content_type_gating.models import ContentTypeGatingConfig
 from lms.djangoapps.courseware.access import has_access
 
